f06506e51093e95f8089267b75ca0c742f0d2162
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-07-24 16:39:15 +0530

    Implement cipher function

--- src/main/scala/CaesarCipher.scala
+++ src/main/scala/CaesarCipher.scala
@@ -1,20 +1,29 @@
 object CaesarCipher {
   def main(args: Array[String]): Unit = {
-    println(encryption("Hello", 0, -1));
-    println(decryption(encryption("Hello", 0, -1), 0, -1))
+    //    println(encryption("Hello", 0, -1));
+    //    println(decryption(encryption("Hello", 0, -1), 0, -1))
+
+    cipher("Hello", encryption, -1);
+    cipher("Gdkkn", decryption, -1);
   }
 
   def encryption(text: String, index: Int, encKey: Int): String = {
     if (index == text.length - 1) {
-      return ((text.charAt(index) + encKey).toChar).toString;
+      return ((text.charAt(index) + encKey).toChar).toString; //added the encryption key to make character change
     }
     return ((text.charAt(index) + encKey).toChar).toString + encryption(text, index + 1, encKey);
   }
 
   def decryption(text: String, index: Int, decKey: Int): String = {
     if (index == text.length - 1) {
-      return ((text.charAt(index) - decKey).toChar).toString;
+      return ((text.charAt(index) - decKey).toChar).toString; //subtracted the encryption key to revise encryption
     }
     return ((text.charAt(index) - decKey).toChar).toString + decryption(text, index + 1, decKey);
   }
+
+  def cipher(text: String, formatFunction: (String, Int, Int) => String, secret: Int): Unit = {
+    println(formatFunction(text, 0, secret))
+  }
+
+
 }
